466e33840ec2cdfbf48b3cec447a116e15e63bd5
ohaz <ohaz@ohaz.engineer>
2018-12-01 09:01:38 +0100

    Day 1 part 2

--- day1/main.py
+++ day1/main.py
@@ -1,9 +1,31 @@
+import itertools
+
+
+def part1(p1_changes):
+    frequency = 0
+
+    for change in p1_changes:
+        frequency += change
+
+    return frequency
+
+
+def part2(p2_changes):
+    frequency = 0
+    old_frequencies = [0]
+    duplicate = None
+
+    for change in itertools.cycle(p2_changes):
+        frequency += change
+        if frequency in old_frequencies:
+            duplicate = frequency
+            break
+        old_frequencies.append(frequency)
+    return duplicate
+
+
 with open('input.txt', 'r') as input_file:
-    changes = input_file.readlines()
+    changes = [int(x) for x in input_file.readlines()]
 
-frequency = 0
-
-for change in changes:
-    frequency += int(change)
-
-print(frequency)
+print(part1(changes))
+print(part2(changes))
