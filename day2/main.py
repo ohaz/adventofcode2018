b5a6b891ccd2af48cc33d0417931e1c4cb5f3430
ohaz <ohaz@ohaz.engineer>
2018-12-02 08:23:07 +0100

    Add day 2 part 2 solution

--- day2/main.py
+++ day2/main.py
@@ -1,3 +1,27 @@
+from functools import lru_cache
+
+
+@lru_cache(maxsize=5000)
+def distance(left, right):
+    counter = 0
+    idx = -1
+    for index, cleft in enumerate(left):
+        if cleft != right[index]:
+            counter += 1
+            idx = index
+        if counter > 1:
+            return None
+    return idx
+
+
+def part2(id_list):
+    for index, input in enumerate(id_list):
+        for other_input in id_list[index:]:
+            dist = distance(input, other_input)
+            if dist and dist >= 0:
+                return input[:dist] + input[dist+1:]
+
+
 def part1(id_list):
     threes = 0
     twos = 0
@@ -23,3 +47,4 @@ with open('input.txt', 'r') as id_file:
     id_lines = id_file.readlines()
 
 print(part1(id_lines))
+print(part2(id_lines))
